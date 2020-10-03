e3bee542e925ba7b3ad37116d466548de694acce
Kevin Burkett <stlbucket@gmail.com>
2020-10-02 20:43:00 -0700

    remove cruft

--- cruft/generate-all-table-scripts.ts
+++
@@ -1,9 +0,0 @@
-import { PgrDbIntrospection, PgrMasterTableScriptSet } from '../../../d'
-import computeAllTableScripts from './compute/compute-table-scripts'
-
-async function generateAllTableScripts(introspection: PgrDbIntrospection): Promise<PgrMasterTableScriptSet> {
-  const computedMasterScriptSet = await computeAllTableScripts(introspection)
-  return computedMasterScriptSet
-}
-
-export default generateAllTableScripts
--- cruft/generate-schema-usage-sql.ts
+++
@@ -1,9 +0,0 @@
-import computeSchemaUsageSql from './compute/compute-schema-usage-sql'
-import writeSchemaUsageSql from './write/write-schema-usage-sql'
-
-async function generateSchemaUsageSql() {
-  const schemaUsageSql = await computeSchemaUsageSql()
-  await writeSchemaUsageSql(schemaUsageSql)
-}
-
-export default generateSchemaUsageSql
